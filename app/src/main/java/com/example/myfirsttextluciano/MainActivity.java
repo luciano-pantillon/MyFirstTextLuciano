25141e2c549073e46be85be87b94b93af71a5719
luciano <lucianopantillon@gmail.com>
2019-09-10 09:40:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myfirsttextluciano/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myfirsttextluciano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Este es mi toast simple", Toast.LENGTH_LONG).show();
+
+
+
+    }
+}
